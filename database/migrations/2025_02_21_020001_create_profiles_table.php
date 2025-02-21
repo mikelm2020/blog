1814eae5f44f03c9befa618aa3a9ec81edd3ae20
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2025-02-20 20:05:56 -0600

    Initial setup for project in PHP with Laravel framework

---
+++ database/migrations/2025_02_21_020001_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo', 255)->nullable();
+
+            //Define the relationship between the user and profile
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
