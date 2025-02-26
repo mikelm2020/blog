d487b9132080c0cc147d34f6fede14da236d0f64
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2025-02-25 19:59:25 -0600

    Added authentication with Laravel ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body>
     <div id="app">
@@ -76,5 +77,6 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
